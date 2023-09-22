558ec3da54f953d149113f1c08c0b37096dab15d
danskor <skorokhodd@yandex.ru>
2023-09-22 13:30:23 +0300

    fix: revision JS code

--- scripts/catalogFavorites.js
+++ scripts/catalogFavorites.js
@@ -1,12 +1,18 @@
 let buttonsFavorites = document.querySelectorAll('.button-favorites');
+let favoriteCounter = document.querySelector('.nav-user-number');
+
+favoriteCounter = Number(favoriteCounter);
+console.log(typeof favoriteCounter);
 
 for (let buttonFavorites of buttonsFavorites) {
 buttonFavorites.onclick = function() {
   buttonFavorites.classList.toggle('button-favorites-current');
   if (buttonFavorites.classList.contains('button-favorites-current')) {
     buttonFavorites.textContent = 'В избранном';
+    favoriteCounter.textContent += 1;
   } else {
       buttonFavorites.textContent = 'В избранное';
+      favoriteCounter.textContent -= 1;
     }
 };
 }
